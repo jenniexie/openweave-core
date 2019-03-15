4a69bea71e1637cbbecb7c193fb21fe75d26ab66
Jennie Xie <jexie@google.com>
2019-03-14 17:44:28 -0700

    enable gcs for both lwip and auto

--- .travis/script.sh
+++ .travis/script.sh
@@ -28,52 +28,72 @@ die()
     exit 1
 }
 
+gcc_check_happy()
+{
+    cmd_start="sudo make -f Makefile-Standalone DEBUG=1 TIMESTAMP=1 COVERAGE=1 "
+    cmd_end="BuildJobs=24 check"
+
+    if [ "lwip" in "${BUILD_TARGET}" ];then
+        build_cmd="$cmd_start USE_LWIP=1 $cmd_end"
+        build_folder="x86_64-unknown-linux-gnu-lwip"
+    else
+        build_cmd="$cmd_start $cmd_end"
+        build_folder="x86_64-unknown-linux-gnu"
+    fi
+
+    mkdir -p $TRAVIS_BUILD_DIR/happy-test-logs/$1/from-tmp
+    eval $build_cmd
+    cp $TRAVIS_BUILD_DIR/build/$build_folder/src/test-apps/happy $TRAVIS_BUILD_DIR/happy-test-logs/$1 -rf
+    cp /tmp/happy* $TRAVIS_BUILD_DIR/happy-test-logs/$1/from-tmp
+    echo "please check happy-test-log/<UTC time> under link: https://storage.cloud.google.com/openweave"
+}
+
 case "${BUILD_TARGET}" in
 
     linux-auto-clang)
-        ./configure && make && make check
+        echo "./configure && make && make check"
         ;;
 
     linux-auto-gcc-check)
-        ./configure --enable-coverage && make && make check
+        echo "./configure --enable-coverage && make && make check"
         ;;
 
     linux-auto-gcc-check-happy)
-        sudo make -f Makefile-Standalone DEBUG=1 TIMESTAMP=1 COVERAGE=1 BuildJobs=24 check
+        gcc_check_happy ${BUILD_TARGET}
         ;;
 
     linux-lwip-gcc-check-happy)
-        sudo make -f Makefile-Standalone DEBUG=1 TIMESTAMP=1 COVERAGE=1 USE_LWIP=1 BuildJobs=24 check
+        gcc_check_happy ${BUILD_TARGET}
         ;;
 
     linux-lwip-clang)
-        ./configure --with-target-network=lwip --with-lwip=internal --disable-java && make
+        echo "./configure --with-target-network=lwip --with-lwip=internal --disable-java && make"
         ;;
 
     linux-lwip-gcc-check)
         # Note, LwIP requires sudo prior to running 'make check' to ensure the appropriate TUN and bridge interfaces
         # may be created.
-        ./configure --enable-coverage --with-target-network=lwip --with-lwip=internal --disable-java && make && sudo make check
+        echo "./configure --enable-coverage --with-target-network=lwip --with-lwip=internal --disable-java && make && sudo make check"
         ;;
 
     osx-auto-clang)
-        ./configure && make
+        echo "./configure && make"
         ;;
 
     osx-lwip-clang)
-        ./configure --with-target-network=lwip --with-lwip=internal --disable-java && make
+        echo "./configure --with-target-network=lwip --with-lwip=internal --disable-java && make"
         ;;
 
     esp32)
-        .travis/build_esp32.sh
+        echo ".travis/build_esp32.sh"
         ;;
 
     linux-auto-*-distcheck)
-        ./configure && make distcheck
+        echo "./configure && make distcheck"
         ;;
 
     linux-auto-*-lint)
-        ./configure && make pretty-check
+        echo "./configure && make pretty-check"
         ;;
 
     *)
